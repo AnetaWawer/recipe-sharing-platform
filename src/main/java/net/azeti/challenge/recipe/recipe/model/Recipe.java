ad9cb1d9bc118fdcef75e2acfb694f0e1b08de0d
Aneta Wawer <anwawer1@gmail.com>
2023-06-13 00:40:42 +0200

    chore: Updated with NotEmpty annotation for title, ingredients and instructions

--- src/main/java/net/azeti/challenge/recipe/recipe/model/Recipe.java
+++ src/main/java/net/azeti/challenge/recipe/recipe/model/Recipe.java
@@ -2,6 +2,7 @@ package net.azeti.challenge.recipe.recipe.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
@@ -16,13 +17,16 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String title;
     private String username;
     @Column(length = 4000)
     private String description;
+    @NotEmpty
     @Column(length = 4000)
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "recipe")
     private List<Ingredients> ingredients;
+    @NotEmpty
     @Column(length = 8000)
     private String instructions;
     private int servings;
