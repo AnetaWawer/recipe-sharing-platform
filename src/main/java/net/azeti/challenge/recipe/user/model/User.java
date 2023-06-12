67757b06003a18c0bc20d6c9077456f3cae2d5bd
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 16:47:48 +0200

    feature: Created User entity

---
+++ src/main/java/net/azeti/challenge/recipe/user/model/User.java
@@ -0,0 +1,20 @@
+package net.azeti.challenge.recipe.user.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
