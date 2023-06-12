375e7e93cf9bc2234412280ec6eb0d2edc356fe0
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 16:21:28 +0200

    feature: created Recipe entity

--- src/main/java/net/azeti/challenge/recipe/recipe/Recipe.java
+++ src/main/java/net/azeti/challenge/recipe/recipe/Recipe.java
@@ -1,14 +1,29 @@
 package net.azeti.challenge.recipe.recipe;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String username;
+    @Column(length = 4000)
     private String description;
-    //... rest of attributes.
+    @Column(length = 4000)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "recipe")
+    private List<Ingredients> ingredients;
+    @Column(length = 8000)
+    private String instructions;
+    private int servings;
 }
