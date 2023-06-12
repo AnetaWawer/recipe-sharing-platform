4ad231c0decab54e3fac270fc2b7f3e905e6db59
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 23:15:10 +0200

    fix: Added missing addFilterBefore

--- src/main/java/net/azeti/challenge/recipe/security/config/SecurityConfiguration.java
+++ src/main/java/net/azeti/challenge/recipe/security/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package net.azeti.challenge.recipe.security.config;
 
 import lombok.RequiredArgsConstructor;
+import net.azeti.challenge.recipe.security.jwt.JwtAuthentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,12 +9,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthentication jwtAuthentication;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +29,7 @@ public class SecurityConfiguration {
                     .requestMatchers(HttpMethod.DELETE, "/api/recipes/**").authenticated()
                 .and()
                 .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthentication, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
