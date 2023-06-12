650724c1b11b75c7ec57bfbe13cd6a32d8ee5c49
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 22:44:02 +0200

    chore: updated Token entity with @Builder

--- src/main/java/net/azeti/challenge/recipe/security/token/Token.java
+++ src/main/java/net/azeti/challenge/recipe/security/token/Token.java
@@ -1,12 +1,12 @@
 package net.azeti.challenge.recipe.security.token;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Token {
     private String accessToken;
 }
