e7117504d10fc93be3f23cf3c684c5974a5097ee
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 20:11:39 +0200

    feature: Implemented endpoint to register new user

--- src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationController.java
+++ src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationController.java
@@ -1,6 +1,8 @@
 package net.azeti.challenge.recipe.security.authentication;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/authentication")
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/register")
+    public void register(@RequestBody Registration registration){
+        authenticationService.register(registration);
+    }
 }
--- src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationService.java
+++ src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationService.java
@@ -1,9 +1,20 @@
 package net.azeti.challenge.recipe.security.authentication;
 
 import lombok.RequiredArgsConstructor;
+import net.azeti.challenge.recipe.user.model.User;
+import net.azeti.challenge.recipe.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    public void register(Registration registration) {
+        User user = User.builder()
+                .username(registration.getUsername())
+                .email(registration.getEmail())
+                .password(registration.getPassword())
+                .build();
+        userRepository.save(user);
+    }
 }
