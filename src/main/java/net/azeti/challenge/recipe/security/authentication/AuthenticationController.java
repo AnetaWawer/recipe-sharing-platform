f2ed9214eddd9a716a209f718eeb88de81eca9c9
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 19:50:00 +0200

    feature: Created controller, service for authentication

---
+++ src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationController.java
@@ -0,0 +1,11 @@
+package net.azeti.challenge.recipe.security.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/authentication")
+public class AuthenticationController {
+}
---
+++ src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationService.java
@@ -0,0 +1,9 @@
+package net.azeti.challenge.recipe.security.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+}
