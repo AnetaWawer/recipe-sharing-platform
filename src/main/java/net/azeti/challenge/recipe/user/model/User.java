5c22e6e9700364d0544293796f5125a4946a0891
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 22:45:47 +0200

    feature: User entity implementing user details

--- src/main/java/net/azeti/challenge/recipe/user/model/User.java
+++ src/main/java/net/azeti/challenge/recipe/user/model/User.java
@@ -2,6 +2,10 @@ package net.azeti.challenge.recipe.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Getter
 @Setter
@@ -10,11 +14,34 @@ import lombok.*;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 }
