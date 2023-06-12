80ca8e7d853b6094939b8fdf98c80ec969c268e2
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 20:45:16 +0200

    chore: implemented password encoder when registering

--- src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationService.java
+++ src/main/java/net/azeti/challenge/recipe/security/authentication/AuthenticationService.java
@@ -3,17 +3,19 @@ package net.azeti.challenge.recipe.security.authentication;
 import lombok.RequiredArgsConstructor;
 import net.azeti.challenge.recipe.user.model.User;
 import net.azeti.challenge.recipe.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     public void register(Registration registration) {
         User user = User.builder()
                 .username(registration.getUsername())
                 .email(registration.getEmail())
-                .password(registration.getPassword())
+                .password(passwordEncoder.encode(registration.getPassword()))
                 .build();
         userRepository.save(user);
     }
