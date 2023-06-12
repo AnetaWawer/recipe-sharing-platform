5e3298e3a7498360b07b025853fcd527e6b64079
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 22:45:05 +0200

    feature: implemented finding by username method

--- src/main/java/net/azeti/challenge/recipe/user/repository/UserRepository.java
+++ src/main/java/net/azeti/challenge/recipe/user/repository/UserRepository.java
@@ -4,8 +4,11 @@ import net.azeti.challenge.recipe.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByEmail(String email);
     boolean existsUserByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
