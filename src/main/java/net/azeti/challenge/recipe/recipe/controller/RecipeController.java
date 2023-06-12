b1290e8923980b24d450db1e045f0009dbe51a69
Aneta Wawer <anwawer1@gmail.com>
2023-06-12 17:12:26 +0200

    feature: Created endpoint to retrieve all recipes

--- src/main/java/net/azeti/challenge/recipe/recipe/controller/RecipeController.java
+++ src/main/java/net/azeti/challenge/recipe/recipe/controller/RecipeController.java
@@ -1,11 +1,21 @@
 package net.azeti.challenge.recipe.recipe.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.azeti.challenge.recipe.recipe.model.Recipe;
+import net.azeti.challenge.recipe.recipe.service.RecipeService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/recipes")
 public class RecipeController {
+    private final RecipeService recipeService;
+    @GetMapping("/")
+    public List<Recipe> getRecipes(){
+        return recipeService.getAllRecipes();
+    }
 }
--- src/main/java/net/azeti/challenge/recipe/recipe/service/RecipeService.java
+++ src/main/java/net/azeti/challenge/recipe/recipe/service/RecipeService.java
@@ -1,9 +1,18 @@
 package net.azeti.challenge.recipe.recipe.service;
 
 import lombok.RequiredArgsConstructor;
+import net.azeti.challenge.recipe.recipe.model.Recipe;
+import net.azeti.challenge.recipe.recipe.repository.RecipeRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeService {
+    private final RecipeRepository recipeRepository;
+    public List<Recipe> getAllRecipes(){
+        return recipeRepository.findAll(Sort.by(Sort.Direction.ASC, "title"));
+    }
 }
