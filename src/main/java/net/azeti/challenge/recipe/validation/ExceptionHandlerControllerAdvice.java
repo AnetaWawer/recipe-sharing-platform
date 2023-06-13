c3685fe54b28dfd331145c49e0048a0337410c59
Aneta Wawer <anwawer1@gmail.com>
2023-06-13 08:36:23 +0200

    feature: Created Exception Handler

---
+++ src/main/java/net/azeti/challenge/recipe/validation/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,32 @@
+package net.azeti.challenge.recipe.validation;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        String errorMessage = "Invalid value provided for Units. Accepted values are: TSP, PC, G, PINCH, TBSP, KG, ML, L.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong credentials. Try again.");
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleEmptyFieldsException(ConstraintViolationException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ensure that title, ingredient, instruction fields are not empty!");
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There is no recipe with given id.");
+    }
+}
