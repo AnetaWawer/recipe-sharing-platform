719e66dcf6be1abe9af7fb41e4d8bfddb58b06a3
Aneta Wawer <anwawer1@gmail.com>
2023-06-13 08:34:37 +0200

    chore: Updated with exception handling

--- src/main/java/net/azeti/challenge/recipe/security/config/SecurityConfiguration.java
+++ src/main/java/net/azeti/challenge/recipe/security/config/SecurityConfiguration.java
@@ -2,12 +2,15 @@ package net.azeti.challenge.recipe.security.config;
 
 import lombok.RequiredArgsConstructor;
 import net.azeti.challenge.recipe.security.jwt.JwtAuthentication;
+import net.azeti.challenge.recipe.validation.CustomizedEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,7 +20,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthentication jwtAuthentication;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
@@ -30,6 +32,9 @@ public class SecurityConfiguration {
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthentication, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomizedEntryPoint(HttpStatus.UNAUTHORIZED, "You need to be logged in to perform this action"))
+                .and()
                 .build();
     }
 }
